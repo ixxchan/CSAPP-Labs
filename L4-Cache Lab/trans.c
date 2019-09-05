8fccd544de6682c84386c536a85b865ac8320a4f
xxchan <xxchan@sjtu.edu.cn>
2019-09-05 21:40:02 +0800

    Part B points increased to 21.2/26 (only 64×64 not passed)

--- L4-Cache Lab/trans.c
+++ L4-Cache Lab/trans.c
@@ -6,7 +6,7 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
@@ -16,15 +16,13 @@ void trans_block(int Bsize, int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, ii, jj;
 
-    for (j = 0; j < M; j+=Bsize) {
-        for (i = 0; i < N; i+=Bsize) {
-            for (ii = i; ii < i+Bsize && ii < N; ++ii)
-                for(jj = j; jj < j+Bsize && jj < M; ++jj)
+    for (j = 0; j < M; j += Bsize)
+        for (i = 0; i < N; i += Bsize)
+            for (ii = i; ii < i + Bsize && ii < N; ++ii)
+                for (jj = j; jj < j + Bsize && jj < M; ++jj)
                     B[jj][ii] = A[ii][jj];
-        }
-    }    
-
 }
+
 void trans_a(int M, int N, int A[N][M], int B[M][N]);
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -36,19 +34,102 @@ void trans_a(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    if(M == 61)
-        trans_block(16, M, N, A, B);
+    int i, j, ii, jj, a0, a1, a2, a3, a4, a5, a6, a7;
+
+    if (M == 61)
+        for (j = 0; j < M; j += 16)
+            for (i = 0; i < N; i += 16)
+                for (ii = i; ii < i + 16 && ii < N; ++ii)
+                    for (jj = j; jj < j + 16 && jj < M; ++jj)
+                        B[jj][ii] = A[ii][jj];
     else if (M == 32)
-        trans_block(8, M, N, A, B);
+        // this idea comes from
+        // https://github.com/Seterplus/CSAPP/blob/master/cachelab/trans.c
+        for (j = 0; j < M; j += 8)
+            for (i = 0; i < N; i += 8)
+                if (i != j)
+                    for (ii = i; ii < i + 8; ++ii)
+                        for (jj = j; jj < j + 8; ++jj)
+                            B[jj][ii] = A[ii][jj];
+                // the same places at A and B shares same set index
+                // which needs to be handled specially
+                // otherwise will cause eviction
+                else
+                {
+                    // first copy
+                    for (ii = i; ii < i + 8; ++ii)
+                    {
+                        // we should load them together, otherwise
+                        // write B will cause eviction
+                        a0 = A[ii][j];
+                        a1 = A[ii][j + 1];
+                        a2 = A[ii][j + 2];
+                        a3 = A[ii][j + 3];
+                        a4 = A[ii][j + 4];
+                        a5 = A[ii][j + 5];
+                        a6 = A[ii][j + 6];
+                        a7 = A[ii][j + 7];
+                        B[ii][j] = a0;
+                        B[ii][j + 1] = a1;
+                        B[ii][j + 2] = a2;
+                        B[ii][j + 3] = a3;
+                        B[ii][j + 4] = a4;
+                        B[ii][j + 5] = a5;
+                        B[ii][j + 6] = a6;
+                        B[ii][j + 7] = a7;
+                    }
+                    // then transpose in place
+                    for (ii = i; ii < i + 8; ++ii)
+                        for (jj = ii; jj < j + 8; ++jj)
+                        {
+                            a0 = B[jj][ii];
+                            B[jj][ii] = B[ii][jj];
+                            B[ii][jj] = a0;
+                        }
+                }
     else
-        trans_block(4, M, N, A, B);
+        for (j = 0; j < M; j += 4)
+            for (i = 0; i < N; i += 4)
+                if (i != j)
+                    for (ii = i; ii < i + 4; ++ii)
+                        for (jj = j; jj < j + 4; ++jj)
+                            B[jj][ii] = A[ii][jj];
+                // the same places at A and B shares same set index
+                // which needs to be handled specially
+                // otherwise will cause eviction
+                else
+                {
+                    // first copy
+                    for (ii = i; ii < i + 4; ++ii)
+                    {
+                        // we should load them together, otherwise
+                        // write B will cause eviction
+                        a0 = A[ii][j];
+                        a1 = A[ii][j + 1];
+                        a2 = A[ii][j + 2];
+                        a3 = A[ii][j + 3];
+                        B[ii][j] = a0;
+                        B[ii][j + 1] = a1;
+                        B[ii][j + 2] = a2;
+                        B[ii][j + 3] = a3;
+                    }
+                    // then transpose in place
+                    for (ii = i; ii < i + 4; ++ii)
+                        for (jj = ii; jj < j + 4; ++jj)
+                        {
+                            a0 = B[jj][ii];
+                            B[jj][ii] = B[ii][jj];
+                            B[ii][jj] = a0;
+                        }
+                }
     // trans_a(M,N,A,B);
+    //assert(is_transpose(M, N, A, B));
 }
 
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
- */ 
+ */
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
@@ -58,13 +139,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 char trans_a_desc[] = "trial";
@@ -72,13 +154,14 @@ void trans_a(int M, int N, int A[N][M], int B[M][N])
 {
     int ii, j, k, Bsize = 8;
 
-    for (j = 0; j < M; j+=Bsize) {
-        for (ii = 0; ii < N; ii++) {
-            for(k = 0; k < Bsize && j + k < M; ++k)
-                B[j+k][ii] = A[ii][j+k];
+    for (j = 0; j < M; j += Bsize)
+    {
+        for (ii = 0; ii < N; ii++)
+        {
+            for (k = 0; k < Bsize && j + k < M; ++k)
+                B[j + k][ii] = A[ii][j + k];
         }
-    }    
-
+    }
 }
 
 /*
@@ -91,11 +174,10 @@ void trans_a(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    // registerTransFunction(trans_a, trans_a_desc); 
-
+    // registerTransFunction(trans_a, trans_a_desc);
 }
 
 /* 
@@ -107,13 +189,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
