21f362a6e13db8805784432cd0a49b1df2f19386
xxchan <xxchan@sjtu.edu.cn>
2019-09-07 15:28:16 +0800

    make progress (total 52.8/53 pts)

--- L4-Cache Lab/trans.c
+++ L4-Cache Lab/trans.c
@@ -12,18 +12,6 @@
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-void trans_block(int Bsize, int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, ii, jj;
-
-    for (j = 0; j < M; j += Bsize)
-        for (i = 0; i < N; i += Bsize)
-            for (ii = i; ii < i + Bsize && ii < N; ++ii)
-                for (jj = j; jj < j + Bsize && jj < M; ++jj)
-                    B[jj][ii] = A[ii][jj];
-}
-
-void trans_a(int M, int N, int A[N][M], int B[M][N]);
 /* 
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -87,43 +75,118 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
                             B[ii][jj] = a0;
                         }
                 }
-    else
-        for (j = 0; j < M; j += 4)
-            for (i = 0; i < N; i += 4)
+    else if (M == 64)
+        // this idea comes from
+        // https://zhuanlan.zhihu.com/p/28585726
+        for (j = 0; j < M; j += 8)
+            for (i = 0; i < N; i += 8)
+            {
                 if (i != j)
+                {
                     for (ii = i; ii < i + 4; ++ii)
                         for (jj = j; jj < j + 4; ++jj)
                             B[jj][ii] = A[ii][jj];
-                // the same places at A and B shares same set index
-                // which needs to be handled specially
-                // otherwise will cause eviction
+                    for (ii = i; ii < i + 4; ++ii)
+                        for (jj = j + 4; jj < j + 8; ++jj)
+                            B[jj - 4][ii + 4] = A[ii][jj];
+                    for (jj = j; jj < j + 4; ++jj)
+                    {
+                        a0 = B[jj][i + 4];
+                        a1 = B[jj][i + 5];
+                        a2 = B[jj][i + 6];
+                        a3 = B[jj][i + 7];
+                        B[jj][i + 4] = A[i + 4][jj];
+                        B[jj][i + 5] = A[i + 5][jj];
+                        B[jj][i + 6] = A[i + 6][jj];
+                        B[jj][i + 7] = A[i + 7][jj];
+                        B[jj + 4][i] = a0;
+                        B[jj + 4][i + 1] = a1;
+                        B[jj + 4][i + 2] = a2;
+                        B[jj + 4][i + 3] = a3;
+                    }
+                    for (ii = i + 4; ii < i + 8; ++ii)
+                        for (jj = j + 4; jj < j + 8; ++jj)
+                            B[jj][ii] = A[ii][jj];
+                }
                 else
                 {
-                    // first copy
-                    for (ii = i; ii < i + 4; ++ii)
+                    for (ii = i; ii < i + 8; ++ii)
                     {
-                        // we should load them together, otherwise
-                        // write B will cause eviction
                         a0 = A[ii][j];
                         a1 = A[ii][j + 1];
                         a2 = A[ii][j + 2];
                         a3 = A[ii][j + 3];
+                        a4 = A[ii][j + 4];
+                        a5 = A[ii][j + 5];
+                        a6 = A[ii][j + 6];
+                        a7 = A[ii][j + 7];
                         B[ii][j] = a0;
                         B[ii][j + 1] = a1;
                         B[ii][j + 2] = a2;
                         B[ii][j + 3] = a3;
+                        B[ii][j + 4] = a4;
+                        B[ii][j + 5] = a5;
+                        B[ii][j + 6] = a6;
+                        B[ii][j + 7] = a7;
                     }
                     // then transpose in place
-                    for (ii = i; ii < i + 4; ++ii)
-                        for (jj = ii; jj < j + 4; ++jj)
+                    for (ii = i; ii < i + 8; ++ii)
+                        for (jj = ii; jj < j + 8; ++jj)
                         {
                             a0 = B[jj][ii];
                             B[jj][ii] = B[ii][jj];
                             B[ii][jj] = a0;
                         }
+                    // // transpose upper left
+                    // for (ii = i; ii < i + 4; ++ii)
+                    //     for (jj = ii; jj < j + 4; ++jj)
+                    //     {
+                    //         a0 = B[jj][ii];
+                    //         B[jj][ii] = B[ii][jj];
+                    //         B[ii][jj] = a0;
+                    //     }
+                    // // transpose upper right
+                    // for (ii = i; ii < i + 4; ++ii)
+                    //     for (jj = ii + 4; jj < j + 8; ++jj)
+                    //     {
+                    //         a0 = B[jj - 4][ii + 4];
+                    //         B[jj - 4][ii + 4] = B[ii][jj];
+                    //         B[ii][jj] = a0;
+                    //     }
+                    // // transpose lower left
+                    // for (ii = i + 4; ii < i + 8; ++ii)
+                    //     for (jj = ii - 4; jj < j + 4; ++jj)
+                    //     {
+                    //         a0 = B[jj + 4][ii - 4];
+                    //         B[jj + 4][ii - 4] = B[ii][jj];
+                    //         B[ii][jj] = a0;
+                    //     }
+                    // // transpose lower right
+                    // for (ii = i + 4; ii < i + 8; ++ii)
+                    //     for (jj = ii; jj < j + 8; ++jj)
+                    //     {
+                    //         a0 = B[jj][ii];
+                    //         B[jj][ii] = B[ii][jj];
+                    //         B[ii][jj] = a0;
+                    //     }
+                    // // swap upper right and lower left
+                    // for (ii = i; ii < i + 4; ++ii)
+                    // {
+                    //     a0 = B[ii + 4][j];
+                    //     a1 = B[ii + 4][j + 1];
+                    //     a2 = B[ii + 4][j + 2];
+                    //     a3 = B[ii + 4][j + 3];
+                    //     B[ii + 4][j] = B[ii][j + 4];
+                    //     B[ii + 4][j + 1] = B[ii][j + 5];
+                    //     B[ii + 4][j + 2] = B[ii][j + 6];
+                    //     B[ii + 4][j + 3] = B[ii][j + 7];
+                    //     B[ii][j + 4] = a0;
+                    //     B[ii][j + 5] = a1;
+                    //     B[ii][j + 6] = a2;
+                    //     B[ii][j + 7] = a3;
+                    // }
                 }
-    // trans_a(M,N,A,B);
-    //assert(is_transpose(M, N, A, B));
+            }
 }
 
 /* 
@@ -149,21 +212,6 @@ void trans(int M, int N, int A[N][M], int B[M][N])
     }
 }
 
-char trans_a_desc[] = "trial";
-void trans_a(int M, int N, int A[N][M], int B[M][N])
-{
-    int ii, j, k, Bsize = 8;
-
-    for (j = 0; j < M; j += Bsize)
-    {
-        for (ii = 0; ii < N; ii++)
-        {
-            for (k = 0; k < Bsize && j + k < M; ++k)
-                B[j + k][ii] = A[ii][j + k];
-        }
-    }
-}
-
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -177,7 +225,7 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    // registerTransFunction(trans_a, trans_a_desc);
+    //registerTransFunction(trans, trans_desc);
 }
 
 /* 
